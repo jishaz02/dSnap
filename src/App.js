f1f4bf485f6bc63d1a35b927625ca84cebd0ed9a
jishaz02 <jishacheriyan02@gmail.com>
2022-08-27 14:04:34 +0530

    added EditProfileScreens

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import CameraScreen from "./screens/CameraScreen";
+import EditNameScreen from "./screens/EditNameScreen";
+import EditUserName from "./screens/EditUserName";
+import EditBirthdayScreen from "./screens/EditBirthdayScreen";
+import EditEmailScreen from "./screens/EditEmailScreen";
 import LoginScreen from "./screens/LoginScreen";
 import MainScreen from "./screens/MainScreen";
 
 function App() {
   return (
     <div>
-      <MainScreen />
+      <EditEmailScreen />
     </div>
   );
 }
---
+++ src/screens/EditNameScreen.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const EditNameScreen = () => {
+    return (
+        <div>
+            <div className='flex items-center border-b p-2 w-screen'>
+                <img
+                    src="https://img.icons8.com/ios-filled/50/388E3C/less-than.png"
+                    alt="Arrow"
+                    className="h-4 "
+                />
+                <p className=" text-green-700 font-bold ">Display Name</p>
+            </div>
+            <p className=' m-2'>This is how you appear on Snapchat, so pick a name
+                your friends know you by.</p>
+            <input type="text" name="Jisha Cheriyan" className='w-full border-b-2 p-2 focus:outline-none' />
+            <p className=" text-blue-700 flex justify-center">Remove name</p>
+        </div>
+
+    )
+}
+
+export default EditNameScreen
